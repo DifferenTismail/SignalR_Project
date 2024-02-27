b94438c8081e9c693c31ffdd824a9e34d70eac98
İsmail Baran <ismailbaran04@gmail.com>
2024-02-27 14:33:11 +0300

    Web UI eklendi

---
+++ SignalR_WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR_WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
