84ad87539415c7d5a164cf2ac0019815f25ee292
İsmail Baran <ismailbaran04@gmail.com>
2024-02-28 15:55:40 +0300

    discount ve contact apileri tamamalandı

---
+++ SignalR_Api/Controllers/ContactController.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR.DtoLayer.ContactDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+        private readonly IMapper _mapper;
+
+        public ContactController(IContactService contactService, IMapper mapper)
+        {
+            _contactService = contactService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var value = _mapper.Map<List<ResultCategoryDto>>(_contactService.TGetListAll());
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createCategoryDto)
+        {
+            _contactService.TAdd(new Contact()
+            {
+                FooterDescription = createCategoryDto.FooterDescription,
+                Location = createCategoryDto.Location,
+                Mail  =createCategoryDto.Mail,
+                Phone = createCategoryDto.Phone
+            });
+            return Ok("İletişim Bilgisi Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteContact(int id)
+        {
+            var value = _contactService.TGetById(id);
+            _contactService.TDelete(value);
+            return Ok("İletişim Bilgisi Silindi");
+        }
+        [HttpGet("GetContact")]
+        public IActionResult GetContact(int id)
+        {
+            var value = _contactService.TGetById(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            _contactService.TUpdate(new Contact()
+            {
+                ContactID = updateContactDto.ContactID,
+                FooterDescription = updateContactDto.FooterDescription,
+                Location = updateContactDto.Location,
+                Mail = updateContactDto.Mail,
+                Phone = updateContactDto.Phone
+            });
+            return Ok("İletişim Bilgisi Güncellendi");
+        }
+    }
+}
