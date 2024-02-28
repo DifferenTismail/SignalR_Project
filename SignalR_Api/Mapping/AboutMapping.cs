177ec7e75f73969d26015d22da3eb08fc080ad92
İsmail Baran <ismailbaran04@gmail.com>
2024-02-28 10:37:34 +0300

    mapping kütüphanesi eklend

--- SignalR_Api/Mapping/AboutMapping.cs
+++ SignalR_Api/Mapping/AboutMapping.cs
@@ -11,7 +11,7 @@ namespace SignalR_Api.Mapping
             CreateMap<About, ResultAboutDto>().ReverseMap();
             CreateMap<About, CreateAboutDto>().ReverseMap();
             CreateMap<About, GetAboutDto>().ReverseMap();
-            CreateMap<About, UpdateAboutDto>().ReverseMap();
+            CreateMap<About, UpdateAboutDto>().ReverseMap(); 
         }
     }
 }
