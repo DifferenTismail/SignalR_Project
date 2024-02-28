12fee3d177fcc9b209cd86f19c42a6e539c5b8b2
İsmail Baran <ismailbaran04@gmail.com>
2024-02-28 17:56:25 +0300

    mapperda güncelleme yapıldı

--- SignalR_Api/Mapping/ProductMapper.cs
+++ SignalR_Api/Mapping/ProductMapper.cs
@@ -13,6 +13,7 @@ namespace SignalR_Api.Mapping
             CreateMap<Product, CreateProductDto>().ReverseMap();
             CreateMap<Product, GetProductDto>().ReverseMap();
             CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, ResultProductWithCategory>().ReverseMap();
         }
     }
 }
