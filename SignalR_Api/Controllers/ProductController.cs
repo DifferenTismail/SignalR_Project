c5b3c6d4b76de2fc6467f9c8b4b19a7d5df97838
İsmail Baran <ismailbaran04@gmail.com>
2024-02-28 16:58:06 +0300

    Ürün ve Feature apileri tamamandı ve program cs e ileride yapılacak olanlar dahil bütün apilerin bağlantısı yapıldı

---
+++ SignalR_Api/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var value = _mapper.Map<List<ResultProductDto>>(_productService.TGetListAll());
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(CreateProductDto createProductDto)
+        {
+            _productService.TAdd(new Product()
+            {
+                Description = createProductDto.Description,
+                ImageUrl = createProductDto.ImageUrl,
+                Price = createProductDto.Price,
+                ProductName = createProductDto.ProductName,
+                ProductStatus = createProductDto.ProductStatus
+                
+            });
+            return Ok("Ürün Bilgisi Eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = _productService.TGetById(id);
+            _productService.TDelete(value);
+            return Ok("Ürün Bilgisi Bilgisi Silindi");
+        }
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _productService.TGetById(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            _productService.TUpdate(new Product()
+            {
+                Description = updateProductDto.Description,
+                ImageUrl = updateProductDto.ImageUrl,
+                Price = updateProductDto.Price,
+                ProductName = updateProductDto.ProductName,
+                ProductStatus = updateProductDto.ProductStatus,
+                ProductID = updateProductDto.ProductID
+            });
+            return Ok("Ürün Bilgisi Güncellendi");
+        }
+    }
+}
--- SignalR_Api/Program.cs
+++ SignalR_Api/Program.cs
@@ -9,26 +9,37 @@ using System.Reflection;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
+//-------------------------------------------------------------
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
-
+//-------------------------------------------------------------
 builder.Services.AddScoped<IAboutService, AboutManager>();
 builder.Services.AddScoped<IAboutDal,EfAboutDal>();
-
+//-------------------------------------------------------------
 builder.Services.AddScoped<IBookingService, BookingManager>();
 builder.Services.AddScoped<IBookingDal, EfBookingDal>();
-
-
+//-------------------------------------------------------------
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
-
+//-------------------------------------------------------------
 builder.Services.AddScoped<IContactService, ContactManager>();
 builder.Services.AddScoped<IContactDal, EfContactDal>();
-
+//-------------------------------------------------------------
 builder.Services.AddScoped<IDiscountService, DiscountManager>();
 builder.Services.AddScoped<IDiscountDal, EfDiscountDal>();
+//-------------------------------------------------------------
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+//-------------------------------------------------------------
+builder.Services.AddScoped<IFeatureService, FeatureManager>();
+builder.Services.AddScoped<IFeatureDal, EfFeatureDal>();
+//-------------------------------------------------------------
+builder.Services.AddScoped<ISocialMediaService, SocialMeadiaManager>();
+builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
+//-------------------------------------------------------------
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+//-------------------------------------------------------------
 
 
 builder.Services.AddControllers();
