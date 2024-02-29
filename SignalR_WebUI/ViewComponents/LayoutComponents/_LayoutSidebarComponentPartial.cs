aa522da53f14e02687c54c07ceb62cc74f24b197
İsmail Baran <ismailbaran04@gmail.com>
2024-02-29 09:19:36 +0300

    view componentler oluşturuldu

---
+++ SignalR_WebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SignalR_WebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
