b5bc8992fdb8db1433ca70a6f8c99efc0c42b015
İsmail Baran <ismailbaran04@gmail.com>
2024-02-29 11:45:30 +0300

    category controller tamamlandı

--- SignalR_WebUI/Controllers/CategoryController.cs
+++ SignalR_WebUI/Controllers/CategoryController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.DtoLayer.CategoryDto;
+using SignalR_WebUI.Dtos.CategoryDtos;
 
 namespace SignalR_WebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+		public CategoryController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMesaj = await client.GetAsync("https://localhost:7178/api/Category");
+            if (responseMesaj.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMesaj.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
