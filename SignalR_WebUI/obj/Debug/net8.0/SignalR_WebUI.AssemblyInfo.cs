7c0408f64811d492b27aaab87d3e8bf8fbe2df94
İsmail Baran <ismailbaran04@gmail.com>
2024-02-29 14:22:35 +0300

    güncellemeler işlendi

--- SignalR_WebUI/obj/Debug/net8.0/SignalR_WebUI.AssemblyInfo.cs
+++ SignalR_WebUI/obj/Debug/net8.0/SignalR_WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR_WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad190c3fcf643245b81291e924405b0ea345ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6536e634309cbe01423d5b1d5f617501e6623ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR_WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR_WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
