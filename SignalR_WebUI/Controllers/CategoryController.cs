37cfc929d9081dacea614e8072b462f14c99e137
İsmail Baran <ismailbaran04@gmail.com>
2024-02-29 09:18:28 +0300

    Kategori controller oluşturuldu

---
+++ SignalR_WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
