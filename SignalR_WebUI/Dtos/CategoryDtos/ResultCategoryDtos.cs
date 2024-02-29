f35dceab082dcd64e3e34249f628e8b71fecf7ac
İsmail Baran <ismailbaran04@gmail.com>
2024-02-29 11:39:18 +0300

    dtoslar eklendş

---
+++ SignalR_WebUI/Dtos/CategoryDtos/ResultCategoryDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR_WebUI.Dtos.CategoryDtos
+{
+	public class ResultCategoryDtos
+	{
+		public int CategoryID { get; set; }
+		public string CategoryName { get; set; }
+		public bool CategoryStatus { get; set; }
+	}
+}
