55d130940ea4c9dd1f47775779488388eb840152
İsmail Baran <ismailbaran04@gmail.com>
2024-02-29 09:45:33 +0300

    navbarı component partiala ekledik

---
+++ SignalR_WebUI/ViewComponents/LayoutComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SignalR_WebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
