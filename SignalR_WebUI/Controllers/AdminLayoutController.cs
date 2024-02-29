6d30e1bd93e4857bb7631e9c199c2d8e953b29a3
İsmail Baran <ismailbaran04@gmail.com>
2024-02-29 09:19:00 +0300

    adminlayoutun admin kısmı için controller oluşturldu

---
+++ SignalR_WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
