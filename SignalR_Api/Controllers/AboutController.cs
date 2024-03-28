afc6c753543fe24cb920d8c81c2038fb0a59463f
İsmail Baran <ismailbaran04@gmail.com>
2024-03-28 17:57:47 +0300

    hakkımızda apisine gelen hatalı veri düzeltildi

--- SignalR_Api/Controllers/AboutController.cs
+++ SignalR_Api/Controllers/AboutController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.DtoLayer.AboutDto;
@@ -11,15 +12,17 @@ namespace SignalR_Api.Controllers
     public class AboutController : ControllerBase
     {
         private readonly IAboutService _aboutService;
+        private readonly IMapper _mapper;
 
-        public AboutController(IAboutService aboutService)
+        public AboutController(IMapper mapper, IAboutService aboutService)
         {
             _aboutService = aboutService;
+            _mapper = mapper;
         }
         [HttpGet]
         public IActionResult AboutList()
         {
-            var values  = _aboutService.TGetListAll();
+            var values = _mapper.Map<List<ResultAboutDto>>(_aboutService.TGetListAll());
             return Ok(values);
         }
         [HttpPost]
@@ -34,7 +37,7 @@ namespace SignalR_Api.Controllers
             _aboutService.TAdd(about);
             return Ok("Hakkımda Kısmı Başarılı Bİr Şekilde Eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteAbout(int id)
         {
             var value = _aboutService.TGetById(id);
@@ -54,12 +57,11 @@ namespace SignalR_Api.Controllers
             _aboutService.TUpdate(about);
             return Ok("Hakkımda Alanı Güncellendi");
         }
-        [HttpGet("GetAbout")]
+        [HttpGet("GetAbout/{id}")]
         public IActionResult GetAbout(int id)
         {
             var value = _aboutService.TGetById(id);
             return Ok(value);
         }
-
     }
 }
