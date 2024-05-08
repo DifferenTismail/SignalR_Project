8579419fce01093b171a8203679dafc8aaa0361c
İsmail Baran <ismailbaran04@gmail.com>
2024-05-08 09:24:13 +0300

    Bokking api controller açıldı

--- SignalR_Api/Controllers/BookingController.cs
+++ SignalR_Api/Controllers/BookingController.cs
@@ -37,7 +37,7 @@ namespace SignalR_Api.Controllers
             _bookingService.TAdd(booking);
             return Ok("Rezervasyon Yapıldı");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteBooking(int id)
         {
             var value = _bookingService.TGetById(id);
@@ -59,7 +59,7 @@ namespace SignalR_Api.Controllers
             _bookingService.TUpdate(booking);
             return Ok("Rezervasyon Güncellendi");
         }
-        [HttpGet("GetBooking")]
+        [HttpGet("{id}")]
         public IActionResult GetBooking(int id)
         {
             var value = _bookingService.TGetById(id);
