9a258062fbe2daebcb5575548d07810b937f8b93
İsmail Baran <ismailbaran04@gmail.com>
2024-05-08 09:37:55 +0300

    Aboutun dtosu eklendi

--- SignalR.DtoLayer/AboutDto/GetAboutDto.cs
+++ SignalR.DtoLayer/AboutDto/GetAboutDto.cs
@@ -8,9 +8,9 @@ namespace SignalR.DtoLayer.AboutDto
 {
     public class GetAboutDto
     {
-        public int AboutID { get; set; }
-        public string ImageUrl { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+		public int AboutID { get; set; }
+		public string ImageUrl { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
     }
 }
