dbca9912ceef886b1a3159d7dc857be5243d18e2
İsmail Baran <ismailbaran04@gmail.com>
2024-05-09 21:58:02 +0300

    order detailin api controllerı açıldı

---
+++ SignalR_Api/Controllers/OrdersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalR_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        [HttpGet("TotalOrderCount")]
+        public IActionResult TotalOrderCount() 
+        {
+            return Ok(_orderService.TTotalOrderCount());
+        }
+
+        [HttpGet("ActiveOrderCount")]
+        public IActionResult ActiveOrderCount()
+        {
+            return Ok(_orderService.TActiveOrderCount());
+        }
+       [HttpGet("LastOrderPrice")]
+        public IActionResult LastOrderPrice()
+        {
+            return Ok(_orderService.TLastOrderPrice());
+        }
+    }
+}
+
